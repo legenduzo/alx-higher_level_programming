215df25641a92b08647a0a0e9e1e8d784d8f20c3
Precious Aleaji <legenduz15@gmail.com>
2023-10-23 12:46:28 +0100

    0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,6 @@ def safe_print_list(my_list=[], x=0):
             else:
                 break
         print("\n")
-    finally:
-        return count
+    except Exception:
+        pass
+    return count
