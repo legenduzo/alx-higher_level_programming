cc470eeee0545443bd4019345f1fc076185ad1a8
Precious Aleaji <legenduz15@gmail.com>
2023-10-28 20:43:28 +0100

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Module documentation for 1-square.py
+
+This module implements size for a square class
+"""
+
+
+class Square:
+    """Represents a square
+
+    A class used to represent a square
+
+    Attributes:
+        size (int): size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
