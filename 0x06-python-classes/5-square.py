e14d154f4126ad27488fe48780925cc801cbef1b
Precious Aleaji <legenduz15@gmail.com>
2023-10-28 23:17:02 +0100

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""Module documentation for 2-square.py
+
+This module implements size for a square class
+"""
+
+
+class Square:
+    """Represents a square
+
+    A class used to represent a square
+
+    Attributes:
+        size (int): size of the square
+    """
+    def __init__(self, size=0):
+        """__init__ method for the square class
+
+        Args:
+            size (int): size must be greater than 0
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """private property with getter and setter
+
+        size has to be > 0 and must be an int
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """Area method
+
+        Returns:
+            The area of the square
+        """
+        return self.size * self.size
+
+    def my_print(self):
+        """Prints # to the standard output
+
+        If self.size = 0, prints nothing
+        """
+        if self.size > 0:
+            for i in range(0, self.size):
+                print('#' * self.size)
+        else:
+            print()
