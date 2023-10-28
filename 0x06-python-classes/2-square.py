1dd6e6c91bf7b6040f2062007499cb9dafb4a86f
Precious Aleaji <legenduz15@gmail.com>
2023-10-28 22:45:11 +0100

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -20,21 +20,21 @@ class Square:
         Args:
             size (int): size must be greater than 0
         """
-        self.__size = size
+        self.size = size
 
-        @property
-        def size(self):
-            """private property with getter and setter
+    @property
+    def size(self):
+        """private property with getter and setter
 
-            size has to be > 0 and must be an int
-            """
-            return self.__size
+        size has to be > 0 and must be an int
+        """
+        return self.__size
 
-        @size.setter
-        def size(self, size):
-            if not isinstance(size, int):
-                raise TypeError('size must be an integer')
-            elif size < 0:
-                raise ValueError('size must be >= 0')
-            else:
-                self.__size = size
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
