a44b103b32572415f67b6dce82abbfae1a9b4fd8
Precious Aleaji <legenduz15@gmail.com>
2023-10-31 01:29:21 +0100

    0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rectangle Module
+
+This module creates a rectangle object
+"""
+
+
+class Rectangle:
+    """Rectangle class
+
+    Creates a rectangle and provides it with attributes and methods
+    """
+    pass
