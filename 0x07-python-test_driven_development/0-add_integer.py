8fc2f27fcc621e532fa4e4fcc783c4b2bb6eac8e
Precious Aleaji <legenduz15@gmail.com>
2023-10-31 23:56:47 +0100

    0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -25,7 +25,3 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/0-add_integer.txt")
