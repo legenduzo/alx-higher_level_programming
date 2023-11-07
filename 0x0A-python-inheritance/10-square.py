ef32441a7153f92db4ea126e0be9158296a5a8e7
Precious Aleaji <legenduz15@gmail.com>
2023-11-07 05:59:49 +0100

    10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Base Geometry module"""
+
+
+class BaseGeometry:
+    """Class with unimplemented area and integer validation"""
+
+    def area(self):
+        """Raise an Exception for unimplemented method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates value as an integer greater than 0.
+        Raises TypeError or ValueError with appropriate messages.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Inherits from BaseGeometry, adds width and height validation"""
+
+    def __init__(self, width, height):
+        """
+        Initialize the Rectangle with width and height after validation.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle inherits from BaseGeometry, with area and printing."""
+
+    def __init__(self, width, height):
+        """
+        Initialize Rectangle with private width and height after validation.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Implements area calculation for the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Provides the string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """Square inherits from Rectangle class, with size property."""
+
+    def __init__(self, size):
+        """
+        Initialize Square with private size after validation.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)  # A square is a rectangle with equal width and height
+
+    def area(self):
+        """Implements area calculation for the square."""
+        return self._Rectangle__width * self._Rectangle__height
