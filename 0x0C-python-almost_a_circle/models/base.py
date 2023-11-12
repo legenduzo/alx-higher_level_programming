9d744978fe0da6b29b5222fbd45a348cce6b63a0
Precious Aleaji <legenduz15@gmail.com>
2023-11-12 21:14:51 +0100

    0

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Base Module
+"""
+
+
+class Base:
+    """Base class
+
+    Attributes:
+        __nb_objects (int): provate class attribute
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """init method
+        
+        Initializes the class
+
+        Args:
+            id: keyword argument
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import unittest
+
+class TestBase(unittest.TestCase):
+
+    def test_id:
+        t = Base(5)
+        self.assertEqual(t.id, 5)
+
+    def test_noid:
+        t = Base()
+        self.assertEqual(t.id, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
