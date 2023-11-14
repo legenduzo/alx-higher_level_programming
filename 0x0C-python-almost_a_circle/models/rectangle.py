6fcd14b086dc8aec39d7b5b23b43dff7477d70e6
Precious Aleaji <legenduz15@gmail.com>
2023-11-14 04:59:42 +0100

    9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -104,15 +104,16 @@ class Rectangle(Base):
         return (f'[Rectangle] ({self.id}) {self.x}/{self.y} '
                 f'- {self.width}/{self.height}')
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Update method"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        keys = ['id', 'width', 'height', 'x', 'y']
+        keyset = set()
+        if args:
+            for key, value in zip(keys, args):
+                if hasattr(self, key):
+                    setattr(self, key, value)
+                    keyset.add(key)
+        if kwargs:
+            for key, value in kwargs.items():
+                if hasattr(self, key) and key not in keyset:
+                    setattr(self, key, value)
