36344a992b80edab73f2e577dc999dc1bc239236
Precious Aleaji <legenduz15@gmail.com>
2023-11-15 03:31:03 +0100

    13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -117,3 +117,13 @@ class Rectangle(Base):
             for key, value in kwargs.items():
                 if hasattr(self, key) and key not in keyset:
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of rectangle"""
+        return {
+                'id': self.id,
+                'width': self.width,
+                'height': self.height,
+                'x': self.x,
+                'y': self.y
+                }
