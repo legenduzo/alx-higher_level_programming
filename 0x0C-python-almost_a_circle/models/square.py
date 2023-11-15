84fd1a3c2ad1a28e8b27db2dec5d8a13afb4b706
Precious Aleaji <legenduz15@gmail.com>
2023-11-15 02:20:56 +0100

    10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Square Module
+
+Inherits from Rectangle
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class
+
+    Has Rectangle as its super class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """Init method
+
+        Initializes new instances of square
+        """
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    def __str__(self):
+        """Str method"""
+        return (f'[Square] ({self.id}) {self.x}/{self.y} '
+                f'- {self.height}')
