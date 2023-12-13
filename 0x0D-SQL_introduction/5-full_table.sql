3c802bbee23bf3e4c4768137b73109f397e12099
Precious Aleaji <legenduz15@gmail.com>
2023-12-13 10:20:17 +0100

    5
    Co-authored-by: P <legenduz15@gmail.com>

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints the full description of the table first_table
+SELECT COLUMN_NAME, COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+AND TABLE_NAME = 'first_table'
