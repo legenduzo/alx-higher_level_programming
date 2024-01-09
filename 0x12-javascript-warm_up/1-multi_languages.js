5b63ef26c61f8f6dfcbb3d02985572629c255ee2
Precious Aleaji <legenduz15@gmail.com>
2024-01-09 04:38:51 +0100

    2

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cIsFun = 'C is fun';
+const pyCool = 'Python is cool';
+const jAmazin = 'JavaScript is amazing';
+console.log(cIsFun);
+console.log(pyCool);
+console.log(jAmazin);
