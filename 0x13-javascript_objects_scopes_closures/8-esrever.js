796ac970e21e8b3fcc1fff585ce151155ba1148b
Precious Aleaji <legenduz15@gmail.com>
2024-01-10 05:59:07 +0100

    8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  const revlist = [];
-  for (const element in list) {
+  let revlist = [];
+  for (const element of list) {
     revlist.unshift(element);
   }
   return revlist;
