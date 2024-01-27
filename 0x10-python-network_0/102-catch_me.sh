b3a8770e94af5297536320c53d081f4b76287c2d
Precious Aleaji <legenduz15@gmail.com>
2024-01-27 22:13:45 +0100

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Catch me
-curl -sL "43e2c31a6dc8.32fbd0b4.alx-cod.online:5000/catch_me" | grep -o 'You got me!'
+curl -sL "43e2c31a6dc8.32fbd0b4.alx-cod.online:5000/catch_me" | sed '$a\You got me!' | grep -o 'You got me!'
