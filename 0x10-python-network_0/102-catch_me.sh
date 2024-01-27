ae272de0b921c7dc28edb2e48c4ce34a63c2fda9
Precious Aleaji <legenduz15@gmail.com>
2024-01-27 21:47:07 +0100

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Catch me
-curl -sLX PUT -H "Origin:School" -d "user_id=98" "43e2c31a6dc8.32fbd0b4.alx-cod.online:5000/catch_me" | grep -o 'You got me!'
+curl -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98" "43e2c31a6dc8.32fbd0b4.alx-cod.online:5000/catch_me" | grep -o 'You got me!'
